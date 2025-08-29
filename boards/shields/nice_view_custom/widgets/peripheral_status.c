ae15eca52a5bc26a52645da3326f57860e88d741
Gabriel Sene <58304709+nonsene@users.noreply.github.com>
2025-08-29 18:08:37 -0300

    Changing animation

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -23,8 +23,22 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include "peripheral_status.h"
 
-LV_IMG_DECLARE(balloon);
-LV_IMG_DECLARE(mountain);
+LV_IMG_DECLARE(frame_000);
+LV_IMG_DECLARE(frame_001);
+LV_IMG_DECLARE(frame_002);
+LV_IMG_DECLARE(frame_003);
+LV_IMG_DECLARE(frame_004);
+LV_IMG_DECLARE(frame_005);
+
+
+const lv_img_dsc_t *anim_imgs[] = {
+    &frame_000,
+    &frame_001,
+    &frame_002,
+    &frame_003,
+    &frame_004,
+    &frame_005,
+};
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
@@ -114,9 +128,17 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_obj_align(top, LV_ALIGN_TOP_RIGHT, 0, 0);
     lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
 
-    lv_obj_t *art = lv_img_create(widget->obj);
-    bool random = sys_rand32_get() & 1;
-    lv_img_set_src(art, random ? &balloon : &mountain);
+    //lv_obj_t *art = lv_img_create(widget->obj);
+    //bool random = sys_rand32_get() & 1;
+    //lv_img_set_src(art, random ? &balloon : &mountain);
+
+    lv_obj_t * art = lv_animimg_create(widget->obj);            //<--
+    lv_obj_center(art);                                         //<--
+    lv_animimg_set_src(art, (const void **) anim_imgs, 12);     //<--
+    lv_animimg_set_duration(art, 4800);                         //<--
+    lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);  //<--
+    lv_animimg_start(art);  
+
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
